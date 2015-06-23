8114bc96ae467e45392079bb37bad6a31b089f72
rub1e <alex.rubner@gmail.com>
2015-06-23 10:48:39 +0100

    functions for ajax calls

--- routes.js
+++ routes.js
@@ -4,8 +4,23 @@ var routes = [
     path: '/',
     method: 'GET',
     handler: handlers.repositories
+  },
+  {
+    method: 'GET',
+    path: '/static/{path*}',
+    handler:  {
+      directory: {
+        path: './'
+      }
+    }
+  },  
+  {
+    path: '/issues',
+    method: 'GET',
+    handler: function(req, res){
+    	res.file("issues.html")
+    }
   }
-
 ];
 
 module.exports = routes;
