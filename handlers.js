f4c7c97365f946bc72fca39c64004cf34f614811
Danielwhyte <danwhy@gmail.com>
2015-06-26 10:32:54 +0100

    aaah

--- handlers.js
+++ handlers.js
@@ -4,7 +4,7 @@ var r = require('rethinkdb');
 var Handlebars = require('handlebars');
 var server = require('./server.js');
 
-r.connect( {host: 'rethink148.aws-us-east-1-data.9.dblayer.com', port: 28015}, function(err, conn) {
+r.connect( {host: '127.0.0.1', port: 28015}, function(err, conn) {
     if (err) {
         console.log('ERROR');
         throw err;
