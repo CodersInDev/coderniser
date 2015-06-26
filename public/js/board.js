040f0ee0e1478e591dfb53d2880c70ec96be537c
Mina <wilhelmina.jane@gmail.com>
2015-06-26 12:00:26 +0100

    fixed problem

--- public/js/board.js
+++ public/js/board.js
@@ -6,34 +6,35 @@ $(function() {
 
     // get html from issues
 
-    gridster = $(".gridster > ul").gridster({
-        widget_margins: [10, 10],
-        widget_base_dimensions: [230, 100],
-        min_cols: 4,
-        draggable : {
-            stop: function(event, ui){
-                lastPosition = gridster.serialize_params();
-                $.ajax({
-                    type: 'POST',
-                    url: '/board',
-                    data: lastPosition
-                });
-
-                // console.log("LAST POSITION");
-                // console.log(JSON.stringify(lastPosition));
-            }
-        },
-        serialize_params: function($w, wgd) {
-            return {
-            id: $($w).attr('id'),
-            col: wgd.col,
-            row: wgd.row,
-            size_x: wgd.size_x,
-            size_y: wgd.size_y,
-
-            };
-        },
-    }).data('gridster');
+    // gridster = $(".gridster > ul").gridster({
+    //     widget_margins: [10, 10],
+    //     widget_base_dimensions: [230, 100],
+    //     min_cols: 4,
+    //     draggable : {
+    //         stop: function(event, ui){
+    //             //lastPosition = gridster.serialize_params();
+    //             lastPosition = gridster.serialize();
+    //             // $.ajax({
+    //             //     type: 'POST',
+    //             //     url: '/board',
+    //             //     data: lastPosition
+    //             // });
+    //
+    //             console.log("LAST POSITION");
+    //             console.log(JSON.stringify(lastPosition));
+    //         }
+    //     },
+    //     serialize_params: function($w, wgd) {
+    //         return {
+    //         id: $($w).attr('id'),
+    //         col: wgd.col,
+    //         row: wgd.row,
+    //         size_x: wgd.size_x,
+    //         size_y: wgd.size_y,
+    //
+    //         };
+    //     },
+    // }).data('gridster');
 
     // var result = gridster.serialize_params;
     //console.log(gridster);
@@ -55,48 +56,59 @@ $(function() {
 
     // ADD NEW ISSUES
 
-//     var json = [
-//   {
-//     "id": "foo",
-//     "col": 1,
-//     "row": 1,
-//     "size_y": 3,
-//     "size_x": 3
-//   },
-//   {
-//     "id": "bar",
-//     "col": 4,
-//     "row": 1,
-//     "size_y": 2,
-//     "size_x": 2
-//   },
-// ];
-//
-// var grid = $("#main").gridster({
-//     widget_margins: [8, 8],
-//     widget_base_dimensions: [140, 140],
-//     serialize_params: function($w, wgd) {
-//         return {
-//             id: wgd.el[0].id,
-//             col: wgd.col,
-//             row: wgd.row,
-//             size_y: wgd.size_y,
-//             size_x: wgd.size_x,
-//         } ;
-//     }
-// }).data('gridster');
-//
-// for(i=0; i<json.length; i++) {
-//     grid.add_widget(
-//         '<div id="' + json[i]['id'] + '"></div>',
-//         json[i]['size_x'],
-//         json[i]['size_y'],
-//         json[i]['col'],
-//         json[i]['row']
-//     );
-// }
-
-
-
+    //     var json = [
+    //       {
+    //         "id": "foo",
+    //         "col": 1,
+    //         "row": 1,
+    //         "size_y": 1,
+    //         "size_x": 1,
+    //       },
+    //       {
+    //         "id": "bar",
+    //         "col": 4,
+    //         "row": 1,
+    //         "size_y": 1,
+    //         "size_x": 1,
+    //       },
+    //     ];
+    // var lastPosition;
+    // var grid = $(".gridster").gridster({
+    //     widget_margins: [8, 8],
+    //     widget_base_dimensions: [230, 100],
+    //     serialize_params: function($w, wgd) {
+    //         return {
+    //             id: wgd.el[0].id,
+    //             col: wgd.col,
+    //             row: wgd.row,
+    //             size_y: wgd.size_y,
+    //             size_x: wgd.size_x,
+    //         } ;
+    //     },
+    //     draggable : {
+    //         stop: function(event, ui){
+    //             lastPosition = grid.serialize();
+    //             $.ajax({
+    //                 type: 'POST',
+    //                 url: '/board',
+    //                 data: lastPosition
+    //             });
+    //
+    //             console.log("LAST POSITION");
+    //             console.log(JSON.stringify(lastPosition));
+    //         }
+    //     },
+    // }).data('gridster');
+    // //console.log(grid.serialize_params);
+    //
+    // for(i=0; i<json.length; i++) {
+    //     grid.add_widget(
+    //         '<div id="' + json[i]['id'] + '"></div>',
+    //         json[i]['size_x'],
+    //         json[i]['size_y'],
+    //         json[i]['col'],
+    //         json[i]['row']
+    //     );
+    // }
 
 });
